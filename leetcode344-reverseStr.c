ffd5ff607bfc6f1230b93d515efc75da0e1c472a
泡泡 <2631520922@qq.com>
2017-10-18 11:47:16 +0800

    Create leetcode344-reverseStr.c
    i don't use another array as target array,but use a temp variable to swap between head and tail

---
+++ leetcode344-reverseStr.c
@@ -0,0 +1,34 @@
+leetcode344. Reverse String
+
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+-------
+
+ 
+#include<stdio.h>  
+#include<string.h>   
+
+char* reverseString(char* s) {
+    int len = strlen(s);
+	//printf("%s  %d\n",s,len);
+    int i=0;
+    char temp;
+    while(i<len/2)
+    {
+    	temp = s[len-1-i];
+    	s[len-1-i] =s[i] ;
+    	s[i] = temp;
+    	i++;
+	}
+    return s;
+}
+int main(){    
+    char str[200] ={"hello1230"};
+   
+	
+    printf("%s\n",reverseString(str));
+	return 0;   
+}    
