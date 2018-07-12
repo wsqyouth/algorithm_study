85ac3fb251ffaa114a37a5f2eadadfcbee4045a0
泡泡 <wsqyouth@163.com>
2018-07-12 11:03:08 +0800

    Create leetcode_dp.cpp

---
+++ leetcode_dp.cpp
@@ -0,0 +1,55 @@
+//一、求最小路径和
+void print2DVect(vector<vector<int> > &res)
+{
+	for (vector<vector<int>>::iterator ite = res.begin(); ite != res.end(); ite++)
+	{
+		vector<int> temp_vect = *ite;
+		for (vector<int>::iterator itee = temp_vect.begin(); itee != temp_vect.end(); itee++)
+			cout << *itee << " ";
+		cout << endl;
+	}
+}
+
+int minimumTotal(vector<vector<int> > &triangle) {
+	if (triangle.empty()) return 0;
+	
+	//初始化dp rows*rows 全为0
+	int rows = triangle.size();
+	vector<vector<int> > dp;
+	for (int i = 0; i < rows; ++i)
+	{
+		dp.push_back(vector<int>(rows, 0));  //每行push一个vector，rows个0
+	}
+	//直接负责最后一行
+	for (int j = 0; j < rows; ++j)
+		dp[rows - 1][j] = triangle[rows - 1][j];
+
+	//由底向上,选取min值+当前Num值
+	for (int i = rows - 2; i >= 0; --i)
+	{
+		for (int j = 0; j <= i; ++j)
+			dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+	}
+
+	
+	return dp[0][0];
+}
+
+int main(){
+
+	int test[][4] = { {2}, {3,4}, {6,5,7}, {4,1,8,3} };
+	vector<vector<int> > tri;
+	for (int i = 0; i < 4; ++i)
+	{
+		tri.push_back(vector<int>());
+		for (int j = 0; j < i + 1; ++j)
+		{
+			tri[i].push_back(test[i][j]);
+		}
+	}
+
+	
+	cout << minimumTotal(tri) << endl;
+
+}
+
