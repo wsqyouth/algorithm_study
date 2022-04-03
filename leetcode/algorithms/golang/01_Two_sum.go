78b7420200efd1cc800102f315ecbf6bc38f35ed
wsqyouth <2631520922@qq.com>
2022-04-04 01:24:09 +0800

    feat: go格式化
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode/algorithms/golang/01_Two_sum.go
+++ leetcode/algorithms/golang/01_Two_sum.go
@@ -5,17 +5,17 @@ import (
 )
 
 func main() {
-	nums :=[]int{3,2,4}
-	fmt.Println(twoSum(nums,6))
-	fmt.Println(twoSumNew(nums,6))
+	nums := []int{3, 2, 4}
+	fmt.Println(twoSum(nums, 6))
+	fmt.Println(twoSumNew(nums, 6))
 }
 
 func twoSum(nums []int, target int) []int {
 	var targetIndex []int
-	for i:=0; i< len(nums)-1;i++{
-		for j:=i+1;j < len(nums); j++{
-			if nums[i] + nums[j] == target {
-				targetIndex = append(targetIndex,i,j)
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				targetIndex = append(targetIndex, i, j)
 				return targetIndex
 			}
 		}
@@ -25,13 +25,13 @@ func twoSum(nums []int, target int) []int {
 
 // 将数据和索引记录下来，遍历时检查是否在map中
 func twoSumNew(nums []int, target int) []int {
-   m := make(map[int]int)
-   for i:=0; i< len(nums); i++{
-	   another:= target - nums[i]
-	   if _,ok := m[another]; ok{
-		   return []int{m[another],i}
-	   }
-	   m[nums[i]] = i
-   }
-   return nil
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
 }
--- leetcode/algorithms/golang/15_3Sum.go
+++ leetcode/algorithms/golang/15_3Sum.go
@@ -7,41 +7,41 @@ import (
 
 func main() {
 	//nums:=[]int{-1,0,1,2,-1,-4}
-	nums:=[]int{-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0}
+	nums := []int{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}
 	fmt.Println(threeSum(nums))
 }
 
 func threeSum(nums []int) [][]int {
-	result := make([][]int,0)
+	result := make([][]int, 0)
 	sort.Ints(nums)
 	length := len(nums)
 	sum := 0
-	for index:=1; index< length-1; index++{
-		start,end:=0,length-1
-		if index > 1 && nums[index] == nums[index-1]{
-			start = index -1
+	for index := 1; index < length-1; index++ {
+		start, end := 0, length-1
+		if index > 1 && nums[index] == nums[index-1] {
+			start = index - 1
 		}
-		for start < index && index < end{
-			if start > 0 && nums[start] == nums[start-1]{
-				start ++
+		for start < index && index < end {
+			if start > 0 && nums[start] == nums[start-1] {
+				start++
 				continue
 			}
-			if end < length-1 && nums[end] == nums[end+1]{
-				end --
+			if end < length-1 && nums[end] == nums[end+1] {
+				end--
 				continue
 			}
 			sum = nums[start] + nums[index] + nums[end]
-			if sum == 0{
-				result = append(result, []int{nums[start],nums[index],nums[end]})
+			if sum == 0 {
+				result = append(result, []int{nums[start], nums[index], nums[end]})
 				start++
-				end --
-			}else if sum > 0{
-				end --
-			}else{
+				end--
+			} else if sum > 0 {
+				end--
+			} else {
 				start++
 			}
 		}
-		
+
 	}
 
 	//fmt.Println(nums)
@@ -50,16 +50,16 @@ func threeSum(nums []int) [][]int {
 func threeSum1(nums []int) [][]int {
 	var target [][]int
 
-	for i:=0; i< len(nums)-2;i++{
-		for j:=i+1;j < len(nums)-1; j++{
-			for k:=j+1; k <len(nums);k++{
-				if nums[i] + nums[j] + nums[k] == 0 {
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[i]+nums[j]+nums[k] == 0 {
 					var targetTemp []int
-					targetTemp= append(targetTemp,nums[i],nums[j],nums[k])
-					fmt.Println(targetTemp,checkExist(target,targetTemp))
-					if !checkExist(target,targetTemp){
-					target = append(target,targetTemp)
-				}
+					targetTemp = append(targetTemp, nums[i], nums[j], nums[k])
+					fmt.Println(targetTemp, checkExist(target, targetTemp))
+					if !checkExist(target, targetTemp) {
+						target = append(target, targetTemp)
+					}
 				}
 			}
 		}
@@ -67,30 +67,27 @@ func threeSum1(nums []int) [][]int {
 	return target
 }
 
-
 // 检查一维数组是否存在于二维数组中
-func checkExist(target [][]int, targetTemp []int) (bool) {
-   for _, arr:= range target {
-   	m := make(map[int]bool)
-   	for i:=0; i< len(arr); i++{
-		m[arr[i]] = true
-	}
-	var count int
-	for _,val := range targetTemp{
-		if _,ok := m[val]; ok{
-			count++
+func checkExist(target [][]int, targetTemp []int) bool {
+	for _, arr := range target {
+		m := make(map[int]bool)
+		for i := 0; i < len(arr); i++ {
+			m[arr[i]] = true
+		}
+		var count int
+		for _, val := range targetTemp {
+			if _, ok := m[val]; ok {
+				count++
+			}
+		}
+		if count == len(targetTemp) {
+			return true
 		}
 	}
-	if count == len(targetTemp){
-		return true
-	}
-   }
-   return false
+	return false
 }
 
-
-
 //总结:
-自己想的办法：数组去重，但是这种办法依赖于判断两个切片是否一致，自己实现的方法有漏洞，针对重复元素使用map判断是错误的
-参考网上是思路，使用双指针法实现，后续继续研究下
-2022年04月03日19:14
+//自己想的办法：数组去重，但是这种办法依赖于判断两个切片是否一致，自己实现的方法有漏洞，针对重复元素使用map判断是错误的
+//参考网上是思路，使用双指针法实现，后续继续研究下
+//2022年04月03日19:14
