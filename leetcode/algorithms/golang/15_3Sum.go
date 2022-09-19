dc01c735118f5d70f820140495109e7576eaea8c
wsqyouth <2631520922@qq.com>
2022-09-19 21:47:28 +0800

    feat: style
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode/algorithms/golang/15_3Sum.go
+++ leetcode/algorithms/golang/15_3Sum.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-// func main() {
-// 	nums:=[]int{-1,0,1,2,-1,-4}
-// 	fmt.Println(threeSum(nums))
-// }
-
 /*
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+}
+
 func threeSum(nums []int) [][]int {
 	result := make([][]int, 0)
 	sort.Ints(nums)
