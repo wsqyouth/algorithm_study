1df546a65f98a2df53e07ccf71f5a14882620753
wsqyouth <2631520922@qq.com>
2022-10-09 09:14:33 +0800

    feat: pow 计算
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode/algorithms/golang/main_dp.go
+++ leetcode/algorithms/golang/main_dp.go
@@ -150,3 +150,23 @@ func maxSubArray(nums []int) int {
 	}
 	return res
 }
+
+// 时间复杂度O(logn),空间复杂度O(1)
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+
+	tmp := myPow(x, n/2)
+	if n%2 == 0 {
+		return tmp * tmp
+	}
+	return tmp * tmp * x
+}
