bd8fa8182fda8e5252361a355abbb3ff97dee780
wsqyouth <2631520922@qq.com>
2022-10-18 09:51:11 +0800

    feat: 不重复元素组合问题
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode/algorithms/golang/main_backtrace.go
+++ leetcode/algorithms/golang/main_backtrace.go
@@ -44,3 +44,25 @@ func permute(nums []int) [][]int {
 	backtrace(nums, option, visited)
 	return ans
 }
+
+// lc78 子集组合问题
+func subsets(nums []int) [][]int {
+	ans := make([][]int, 0)
+	// 记录选择
+	option := make([]int, 0)
+	// 使用闭包是不想返回res了，比较麻烦
+	var backtrace func(nums []int, start int, option []int)
+	backtrace = func(nums []int, start int, option []int) {
+		ans = append(ans, append([]int{}, option...))
+		for i := start; i < len(nums); i++ {
+			// 做选择
+			option = append(option, nums[i])
+			// 进入下一层决策树
+			backtrace(nums, i+1, option)
+			// 取消选择
+			option = option[:len(option)-1]
+		}
+	}
+	backtrace(nums, 0, option)
+	return ans
+}
