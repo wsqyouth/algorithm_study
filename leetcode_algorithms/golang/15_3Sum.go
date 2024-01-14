334bfc1cb0ba9d722c982684487c2fcf79e094c3
wsqyouth <2631520922@qq.com>
2024-01-14 21:48:08 +0800

    feat: 接近3数之和
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode_algorithms/golang/15_3Sum.go
+++ leetcode_algorithms/golang/15_3Sum.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 /*
@@ -92,3 +93,41 @@ func checkExist(target [][]int, targetTemp []int) bool {
 //改进：在添加数组前可以排序去重
 //参考网上是思路，使用双指针法实现，后续继续研究下
 //2022年04月03日19:14
+
+// lc 16. 最接近的三数之和
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	ans := nums[0] + nums[1] + nums[n-1]
+
+	for i := 0; i < n-2; i++ {
+		n1 := nums[i]
+		left, right := i+1, n-1
+		for left < right {
+			n2, n3 := nums[left], nums[right]
+			sum := n1 + n2 + n3
+			if sum > target {
+				right--
+			} else {
+				left++
+			}
+			if abs(sum-target) < abs(ans-target) {
+				ans = sum
+			}
+		}
+	}
+	return ans
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+/*
+首先对输入的数组进行了排序，然后通过固定一个数，移动两个指针来找到最接近目标值的三个数的和。
+如果当前的和大于目标值，就将右指针向左移动；如果当前的和小于目标值，就将左指针向右移动。
+在每次计算和的时候，都会检查当前的和是否比之前找到的更接近目标值，如果是，就更新结果。
+*/
