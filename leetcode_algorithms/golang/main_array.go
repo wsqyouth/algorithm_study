0ad6951227b593ca78a2ef94df85b836537eeced
wsqyouth <2631520922@qq.com>
2024-01-21 16:44:19 +0800

    feat: 最长回文字符串
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode_algorithms/golang/main_array.go
+++ leetcode_algorithms/golang/main_array.go
@@ -249,3 +249,32 @@ func longestConsecutive(nums []int) int {
 	}
 	return res
 }
+
+// lc5 最长回文子串
+func longestPalindrome(s string) string {
+	if s == "" {
+		return s
+	}
+
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	n := len(s)
+	for left >= 0 && right < n && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1 //这个比较难记
+}
