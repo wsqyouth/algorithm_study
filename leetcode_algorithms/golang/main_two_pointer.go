60cb806c4d4a93192b6c444cccde05c669b254d3
wsqyouth <2631520922@qq.com>
2026-03-18 17:46:22 +0800

    feat: n sum
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- leetcode_algorithms/golang/main_two_pointer.go
+++ leetcode_algorithms/golang/main_two_pointer.go
@@ -5,17 +5,16 @@ import (
 	"sort"
 )
 
-/*
 func main() {
 	sumTest()
 }
-*/
+
 func sumTest() {
-	nums := []int{2, 7, 11, 15}
-	fmt.Println(twoSum(nums, 9))
-	fmt.Println(twoSumNew(nums, 9))
-	nums = []int{-1, 0, 1, 2, -1, -4}
-	fmt.Println(threeSum(nums))
+	// nums := []int{2, 7, 11, 15}
+	// fmt.Println(twoSum(nums, 9))
+	// fmt.Println(twoSumNew(nums, 9))
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSumNew(nums))
 }
 
 // lc1 两数之和
@@ -80,3 +79,70 @@ func threeSum(nums []int) [][]int {
 	}
 	return result
 }
+
+func threeSumNew(nums []int) [][]int {
+	var res [][]int
+	sort.Ints(nums)
+	n := len(nums)
+	// i left right
+	for i := 0; i < n-2; i++ {
+		// 对 i 进行去重
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, n-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				// 对left和right进行去重
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum > 0 {
+				right-- // 和太大，右指针左移减小
+			} else {
+				left++ // 和太小，左指针右移增大
+			}
+		}
+	}
+	return res
+}
+
+func threeSumClosestNew(nums []int, target int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	closestSum := nums[0] + nums[1] + nums[n-1]
+
+	for i := 0; i < n-2; i++ {
+		left, right := i+1, n-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum > target {
+				right--
+			} else {
+				left++
+			}
+			if abs(sum-target) < abs(closestSum-target) {
+				closestSum = sum
+			}
+		}
+	}
+	return closestSum
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+/*
+首先对输入的数组进行了排序，然后通过固定一个数，移动两个指针来找到最接近目标值的三个数的和。如果当前的和大于目标值，就将右指针向左移动；如果当前的和小于目标值，就将左指针向右移动。在每次计算和的时候，都会检查当前的和是否比之前找到的更接近目标值，如果是，就更新结果。
+*/
